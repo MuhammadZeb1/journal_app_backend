05556c3b0d3121ab7252893efc4764f44090e975
MuhammadZeb1 <zebafridiuu@gmail.com>
2026-01-16 13:08:27 +0500

    change the url

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:"https://journal-app-frontend-five.vercel.app",
     credentials: true,
   })
 );
